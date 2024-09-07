0398e2b5d1140968b8641fac7a7b7f7a52c0dbe3
JereGDev <jeregamesdev@gmail.com>
2024-09-07 18:18:51 +0100

    Fixed: Jump can now change directions

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,7 +6,6 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta):
 
-	var directionx = Input.get_axis("ui_left", "ui_right")
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -14,16 +13,17 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY 
+		velocity.y = JUMP_VELOCITY
 	
 	#Asks if it is on floor to be able to move in X vector
-	if is_on_floor():
-		if directionx:
-			#Moves the player
-			velocity.x = directionx * SPEED
-		else:
-			#End the movement
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var directionx = Input.get_axis("ui_left", "ui_right")
+	if directionx:
+		#Moves the player
+		velocity.x = directionx * SPEED
+	else:
+		#End the movement
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 	Animated()
 
