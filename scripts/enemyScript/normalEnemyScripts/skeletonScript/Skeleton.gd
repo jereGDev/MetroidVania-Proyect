932ec9a7135e697e3857cf2a8501b8cae6bb50c8
JereGDev <jeregamesdev@gmail.com>
2024-09-23 14:08:38 +0100

    Fixed: Skeleton gravity

--- scripts/enemyScript/normalEnemyScripts/skeletonScript/Skeleton.gd
+++ scripts/enemyScript/normalEnemyScripts/skeletonScript/Skeleton.gd
@@ -5,11 +5,21 @@ var playerChase:bool = false
 var player = null
 @onready var animations = $AnimatedSprite2D
 
+func _ready():
+	player = get_node("/root/BaseScene/Player/Player")
+	if player:
+		print("PILLA AL JUGADOR")
+
 #Handle skeleton movement
 func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
 	if playerChase:
 		position += (player.position - position) / SPEED
 	move_and_slide()
+	animatedEnemy()
 
 #Handle player detection for movement when players enters the area
 func _on_detection_area_body_entered(body):
@@ -21,15 +31,19 @@ func _on_detection_area_body_entered(body):
 
 #Handle player detection for movement when players leaves the area
 func _on_detection_area_body_exited(body):
-	pass # Replace with function body.
+	if body.name == "player":
+		playerChase = false
+	else:
+		return
 
 #Handle the skeleton animation
 func animatedEnemy():
-	if velocity.x:
-		animations.play("walk")
-		if velocity.x < 0:
-			animations.flip_h = true
-		if velocity.x > 0:
-			animations.flip_h = false
-	else:
-		animations.play("Idle")
+	if is_on_floor():
+		if velocity.x:
+			animations.play("walk")
+			if velocity.x < 0:
+				animations.flip_h = true
+			if velocity.x > 0:
+				animations.flip_h = false
+		else:
+			animations.play("Idle")
