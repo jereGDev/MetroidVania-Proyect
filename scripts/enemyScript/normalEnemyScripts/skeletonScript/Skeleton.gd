7de78b8a5b63333cd0b6eb548d1c9a92388ed78e
JereGDev <jeregamesdev@gmail.com>
2024-10-04 14:28:38 +0100

    Feature: Skeleton can now attack player

--- scripts/enemyScript/normalEnemyScripts/skeletonScript/Skeleton.gd
+++ scripts/enemyScript/normalEnemyScripts/skeletonScript/Skeleton.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 const SPEED:float = 30.0
 var playerChase:bool = false
-var skeletonAttack:int = 10
+var skeletonDamage:int = 3
 var skeletonCanAttack:bool = false
 var skeletonLife:int = 15
+var skeletonAttackRange:float = 30.0
+var isSkeletonAttacking:bool = false
 var player = null
 @onready var animations = $AnimatedSprite2D
 
@@ -12,6 +14,7 @@ var player = null
 #This way player is not null
 func _ready():
 	player = get_node("/root/TestScene/Player")
+	animations.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 #Handle skeleton movement
 func _physics_process(delta):
@@ -22,6 +25,9 @@ func _physics_process(delta):
 	if playerChase:
 		var direction = (player.position - position).normalized()
 		velocity.x = direction.x * SPEED
+		
+		if position.distance_to(player.position) <= skeletonAttackRange:
+			Attack()
 		
 	move_and_slide()
 	animatedEnemy()
@@ -42,16 +48,32 @@ func _on_detection_area_body_exited(body):
 	else:
 		return
 
+
+
+# Function to handle the attack
+func Attack() -> void:
+	if player != null and not isSkeletonAttacking:
+		isSkeletonAttacking = true
+		player.playerLife -= skeletonDamage 
+		print("Vida del jugador: ", player.playerLife)
+		animations.play("attack")
+
 #Handle the skeleton animation
 func animatedEnemy():
 	if is_on_floor():
 		#Ask if the absolute value of velocity in X is different than 0;
-		if abs(velocity.x) > 0:
-			animations.play("walk")
-			#Depending on X is greater or lower than 0, it will flip the sprite;
-			if velocity.x < 0:
-				animations.flip_h = true
-			if velocity.x > 0:
-				animations.flip_h = false
-		else:
-			animations.play("Idle")
+		if !isSkeletonAttacking:
+			if abs(velocity.x) > 0:
+				animations.play("walk")
+				#Depending on X is greater or lower than 0, it will flip the sprite;
+				if velocity.x < 0:
+					animations.flip_h = true
+				if velocity.x > 0:
+					animations.flip_h = false
+			else:
+				animations.play("Idle")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if isSkeletonAttacking:
+		isSkeletonAttacking = false
